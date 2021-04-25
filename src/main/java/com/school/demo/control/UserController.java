8f7bc41cbbaf285060990450f0d59cb6abec5783
DeLL-5459 <1191114962@qq.com>
2021-04-26 01:04:41 +0800

    修复了一些问题

--- src/main/java/com/school/demo/control/UserController.java
+++ src/main/java/com/school/demo/control/UserController.java
@@ -10,10 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.UUID;
@@ -35,22 +33,27 @@ public class UserController {
      */
     @PostMapping("wx/login")
     @ResponseBody
-    public GlobalResult user_login(@RequestParam(value = "code", required = false) String code,
-                                       @RequestParam(value = "rawData", required = false) String rawData,
-                                       @RequestParam(value = "signature", required = false) String signature) {
+    public GlobalResult user_login(@RequestBody UserRequest userRequest) {
+        String code = userRequest.getCode();
+        WxUserRequest rawData = userRequest.getRawData();
+        log.info("rawData---{}",JSON.toJSONString(rawData));
+
+        String signature = userRequest.getSignature();
         log.info("code---------{}",code);
         // 用户非敏感信息：rawData
-        // 签名：signature
-        JSONObject rawDataJson = JSON.parseObject(rawData);
         // 1.接收小程序发送的code
         // 2.开发者服务器 登录凭证校验接口 appi + appsecret + code
         JSONObject SessionKeyOpenId = WechatUtil.getSessionKeyOrOpenId(code);
+        log.info("SessionKeyOpenId---------{}",SessionKeyOpenId);
         // 3.接收微信接口服务 获取返回的参数
         String openid = SessionKeyOpenId.getString("openid");
         String sessionKey = SessionKeyOpenId.getString("session_key");
-
+        if (null==openid||"".equals(openid)){
+            return GlobalResult.build(500, "openId解析失败", null);
+        }
         // 4.校验签名 小程序发送的签名signature与服务器端生成的签名signature2 = sha1(rawData + sessionKey)
-        String signature2 = DigestUtils.sha1Hex(rawData + sessionKey);
+        String signature2 = DigestUtils.sha1Hex(JSON.toJSONString(rawData) + sessionKey);
+        log.info("这是signature---{}，这是signature2----{}",signature,signature2);
         if (!signature.equals(signature2)) {
             return GlobalResult.build(500, "签名校验失败", null);
         }
@@ -60,25 +63,18 @@ public class UserController {
         String skey = UUID.randomUUID().toString();
         if (user == null) {
             // 用户信息入库
-            String nickName = rawDataJson.getString("nickName");
-            String avatarUrl = rawDataJson.getString("avatarUrl");
-            String gender = rawDataJson.getString("gender");
-            String city = rawDataJson.getString("city");
-            String country = rawDataJson.getString("country");
-            String province = rawDataJson.getString("province");
-
             user = new WxUser();
             user.setOpenId(openid);
             user.setSkey(skey);
             user.setCreateTime(new Date());
             user.setLastVisitTime(new Date());
             user.setSessionKey(sessionKey);
-            user.setCity(city);
-            user.setProvince(province);
-            user.setCountry(country);
-            user.setAvatarUrl(avatarUrl);
-            user.setGender(Integer.parseInt(gender));
-            user.setNickName(nickName);
+            user.setCity(rawData.getCity());
+            user.setProvince(rawData.getProvince());
+            user.setCountry(rawData.getCountry());
+            user.setAvatarUrl(rawData.getAvatarUrl());
+            user.setGender(rawData.getGender());
+            user.setNickName(rawData.getNickName());
 
             this.userMapper.insert(user);
         } else {
